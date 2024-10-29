d7d462f658f9b9f44c08387315ccc953ad50183a
“yatkun” <sweetninenty99@gmail.com>
2024-10-29 16:41:23 +0800

    update 1.2

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
-
+    @livewireStyles
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
 
--- resources/views/components/layouts/template.blade.php
+++ resources/views/components/layouts/template.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ?? config('app.name') }}</title>
+    @livewireStyles
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
